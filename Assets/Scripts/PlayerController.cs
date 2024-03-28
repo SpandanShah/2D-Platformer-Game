ccf3ddebde0286a39dd2cdbb6c5451df39277608
Spandan Shah <spandan.s@ahduni.edu.in>
2024-03-28 12:27:14 +0530

    This is player movement and Jump movement(using space/not vertical input)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour
     public Animator animator;
     private BoxCollider2D collider;
     private Rigidbody2D rigidbody2D;
+    public float speed;
     private void Awake()
     {
         Debug.Log("Player Controller awake");
@@ -16,36 +17,29 @@ public class PlayerController : MonoBehaviour
     //     Debug.Log("Collision: "+ collision.gameObject.name);
     // }
 
-    void Start()
+    /*void Start()
     {
         animator = GetComponent<Animator>();
         collider = GetComponent<BoxCollider2D>();
         rigidbody2D = GetComponent<Rigidbody2D>();
-    }
+    }*/
 
     private void Update()
     {
-        float speed = Input.GetAxisRaw("Horizontal");
-        animator.SetFloat("Speed", Mathf.Abs(speed));
-        Vector3 scale = transform.localScale;
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        bool VerticalInput = Input.GetKeyDown(KeyCode.Space);
+        float vertical = Input.GetAxisRaw("Jump");
 
-        if (speed < 0)
-        {
-            scale.x = -1f * Mathf.Abs(scale.x);
-        }
-        else if (speed > 0)
-        {
-            scale.x = Mathf.Abs(scale.x);
-        }
-        transform.localScale = scale;
+        MoveCharactor(horizontal);
+        PlayerMovementAnimation(horizontal, VerticalInput);
+        
 
-        bool isJumping = false;
+        /*bool isJumping = false;
 
         if (Input.GetKeyDown(KeyCode.UpArrow) && !isJumping)
         {
             isJumping = true;
             animator.SetBool("Jump", true);
-            
         }
         if (animator.GetCurrentAnimatorStateInfo(0).IsName("Jump") && isJumping && animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1f)
         {
@@ -63,6 +57,43 @@ public class PlayerController : MonoBehaviour
         {
             isCrouching = false;
             animator.SetBool("Crouch", false); // Transition to idle or walk state
+        }*/
+    }
+    private void MoveCharactor(float horizontal)
+    {
+        Vector3 position = transform.position;
+        position.x += horizontal * speed * Time.deltaTime;
+        transform.position = position;
+    }
+    private void PlayerMovementAnimation(float horizontal, bool VerticalInput)
+    {
+        animator.SetFloat("Speed", Mathf.Abs(horizontal));
+        Vector3 scale = transform.localScale;
+
+        if (horizontal < 0)
+        {
+            scale.x = -1f * Mathf.Abs(scale.x);
         }
+        else if (horizontal > 0)
+        {
+            scale.x = Mathf.Abs(scale.x);
+        }
+        transform.localScale = scale;
+
+        //Jump
+        if (VerticalInput)
+        {
+            animator.SetTrigger("JumpTr");
+        }
+        /*
+        if ( vertical > 0)
+        {
+            animator.SetBool("Jump", true);
+        }
+        else
+        {
+            animator.SetBool("Jump", false);
+        }*/
+        
     }
 }
