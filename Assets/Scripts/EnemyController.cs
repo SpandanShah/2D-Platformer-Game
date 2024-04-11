b63b7d51b01d7b35abc280603b158e04c98cc721
Spandan Shah <spandan.s@ahduni.edu.in>
2024-04-11 15:54:38 +0530

    Life Allocation to Ellen

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -2,15 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class EnemyController : MonoBehaviour
 {
+    [SerializeField] public int TouchCount = 0;
+    public GameObject life1;
+    public GameObject life2;
+    public GameObject life3;
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.GetComponent<PlayerController>() != null)
+        TouchCount++;
+        Debug.Log("Touch count: " + TouchCount);
+        if (TouchCount == 1)
+        {
+            Debug.Log("Destroy Life 3");
+            Destroy(life3);
+        }
+        else if (TouchCount == 2)
+        {
+            Destroy(life2);
+        }
+        else if (TouchCount == 3)
+        {
+            Destroy(life1);
+        }
+        if (collision.gameObject.GetComponent<PlayerController>() != null && TouchCount == 3)
         {
             PlayerController playerController = collision.gameObject.GetComponent<PlayerController>();
-
             playerController.KillPlayer();
+            TouchCount = 0;
         }
     }
 }
